b1e4da32d5b67831c3f953b0d115222d21241639
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2019-03-21 21:44:10 -0700

    class based views, home page and docs page

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 # pages/urls.py
 
 from django.urls import path
-from .views import homePageView
+from .views import HomePageView, DocsPageView
 
 urlpatterns = [
-    path("", homePageView, name="home")
+    path("", HomePageView.as_view(), name="home"),
+    path("docs/", DocsPageView.as_view(), name="docs")
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,11 @@
 # pages/views.py
 
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
 
-def homePageView(request):
-    return HttpResponse('Django IoT Server')
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class DocsPageView(TemplateView):
+    template_name = 'docs.html'
