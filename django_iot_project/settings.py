bce440fb62b19c30a6862326249de5f78e11a5f8
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2019-03-22 18:18:28 -0700

    static files in templates by default?

--- django_iot_project/settings.py
+++ django_iot_project/settings.py
@@ -74,6 +74,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'builtins': [
+                'django.contrib.staticfiles.templatetags.staticfiles',
+            ],
         },
     },
 ]
