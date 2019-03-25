92897748ae25339d2a3235765110427277def447
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2019-03-25 14:13:19 -0700

    simplified the data model

--- data/models.py
+++ data/models.py
@@ -5,15 +5,13 @@ from django.db import models
 # Create your models here.
 
 class Data(models.Model):
-    DATA_TYPE_CHOICES = (('flt', "Float"), ('str', "string"))
     created_at = models.DateTimeField(auto_now_add=True)
     channel_num = models.PositiveSmallIntegerField()
     field_num = models.PositiveSmallIntegerField()
-    data_flt = models.DecimalField(max_digits=19, decimal_places=10)
-    data_str = models.CharField(max_length=20)
+    data = models.DecimalField(max_digits=19, decimal_places=10)
     uploaded_by = models.CharField(max_length=30)
     data_pt_num = models.PositiveIntegerField()
-    data_pt_type = models.CharField(max_length=3, choices=DATA_TYPE_CHOICES, default='flt')
+    upload_success = models.BooleanField(default=True)
 
     def __str__(self):
-        return f'{self.created_at} channel: {self.channel_num} field: {self.field_num} data: {self.data_flt}'
+        return f'{self.created_at} channel: {self.channel_num} field: {self.field_num} data: {self.data}'
