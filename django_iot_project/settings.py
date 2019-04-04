2fe63d8e47b5cdef16bad94d2dfb3bbcd9d84026
SYAM101u04 <SYAM101u04@ad.pcc.edu>
2019-04-04 10:43:47 -0700

    added Django humanize contrib to settings.py for dates

--- django_iot_project/settings.py
+++ django_iot_project/settings.py
@@ -33,12 +33,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     
     # Project specific apps
     'pages.apps.PagesConfig',
