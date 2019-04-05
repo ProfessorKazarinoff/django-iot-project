d4503c3e56d4c973ed032cd42fb7f01db410ea75
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2019-04-05 12:27:56 -0700

    building channel listing page

--- channels/views.py
+++ channels/views.py
@@ -11,14 +11,21 @@ from django.shortcuts import get_object_or_404
 from .filters import ChannelFilter
 
 class ChannelListView(ListView):
-
-    model = IotData
+    # don't need model attribute defined if queryset attribute is defined
+    # model = IotData
     template_name = "one_channel_listing.html"
-    slug_url_kwarg = "channel_id"
 
+    # Try to pull out the channel number from the URL
+
+
+    # specify the list of object from the model with by defining a queryset
+    # queryset = IotData.objects.all()
     
+    # speficy a subset of the object from the model by defining a query set with a filter
+    queryset = IotData.objects.filter(channel_num__exact=5).order_by('created_at').reverse()
 
+    #add extra variable into template
     def get_context_data(self, *args, **kwargs):
         context = super(ChannelListView, self).get_context_data(*args, **kwargs)
-        context['channel_number'] = 2
+        context['channel_number'] = 5
         return context
