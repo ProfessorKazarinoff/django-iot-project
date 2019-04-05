2e6eb6eeb70083b116621f61ee6d04778b08877d
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2019-04-04 19:54:03 -0700

    trying function based views

--- channels/urls.py
+++ channels/urls.py
@@ -6,3 +6,13 @@ from .views import ChannelListView
 urlpatterns = [
     path("list/", ChannelListView.as_view(), name="datalist"),
 ]
+
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#     path('api/entry/create/<entryAttribute1>/<entryAttribute2>',views.createmydatabaseentry,name='create'),
+#     path('api/entry/<entryID>',views.showmydatabaseentry,name='show'),
+#     path('api/entry/index',views.indexdatabaseentrys,name='index'),
+  
+# ]
