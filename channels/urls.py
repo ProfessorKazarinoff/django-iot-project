5869e7c24219b0f139baf3910e4b1135a50220d7
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2019-04-05 17:22:20 -0700

    added single channel and single channel and single field pages

--- channels/urls.py
+++ channels/urls.py
@@ -1,11 +1,11 @@
 # channels/urls.py
 
 from django.urls import path
-from .views import ChannelListView #, createmydatabaseentry
+from .views import ChannelListView, FieldListView
 
 urlpatterns = [
     path("list/", ChannelListView.as_view(), name="datalist"),
     path('list/<slug:channel_id>/', ChannelListView.as_view(), name='channel_list'),
     path('<slug:channel_id>/', ChannelListView.as_view(), name='one_channel_list'),
-    #path('entry/create/channel/<entryAttribute1>/field/<entryAttribute2>/data/<entryAttribute3>', createmydatabaseentry, name='create'),
+    path('<slug:channel_id>/field/<slug:field_id>/', FieldListView.as_view(), name='one_channel_list'),
 ]
--- channels/views.py
+++ channels/views.py
@@ -1,31 +1,40 @@
 # channels/views.py
 
-from django.shortcuts import render
-# what we're going to return when a request is sent
-from django.http import JsonResponse
 from django.views.generic import ListView
-from .models import Channels
 from iot_data.models import IotData
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
-from .filters import ChannelFilter
 
 class ChannelListView(ListView):
-    # don't need model attribute defined if queryset attribute is defined
-    # model = IotData
+
     template_name = "one_channel_listing.html"
 
-    # Try to pull out the channel number from the URL
-
-
-    # specify the list of object from the model with by defining a queryset
-    # queryset = IotData.objects.all()
-    
-    # speficy a subset of the object from the model by defining a query set with a filter
-    queryset = IotData.objects.filter(channel_num__exact=5).order_by('created_at').reverse()
+    def get_queryset(self):
+        channel_number = self.kwargs.get("channel_id")
+        queryset = IotData.objects.filter(channel_num__exact=channel_number).order_by('created_at').reverse()
+        return queryset
 
     #add extra variable into template
     def get_context_data(self, *args, **kwargs):
+        channel_number = self.kwargs.get("channel_id")
         context = super(ChannelListView, self).get_context_data(*args, **kwargs)
-        context['channel_number'] = 5
+        context['channel_number'] = channel_number
+        return context
+
+
+class FieldListView(ListView):
+
+    template_name = "one_field_listing.html"
+
+    def get_queryset(self):
+        channel_number = self.kwargs.get("channel_id")
+        field_number = self.kwargs.get("field_id")
+        queryset = IotData.objects.filter(channel_num__exact=channel_number).filter(field_num__exact=field_number).order_by('created_at').reverse()
+        return queryset
+
+    #add extra variable into template
+    def get_context_data(self, *args, **kwargs):
+        channel_number = self.kwargs.get("channel_id")
+        field_number = self.kwargs.get("field_id")
+        context = super(FieldListView, self).get_context_data(*args, **kwargs)
+        context['channel_number'] = channel_number
+        context['field_number'] = field_number
         return context
