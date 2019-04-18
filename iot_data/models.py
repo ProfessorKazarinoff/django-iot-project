1870672f293e95036ea29436c02572cf64342c77
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2019-04-18 08:04:18 -0700

    removed a comment and field from IoT Data model

--- iot_data/models.py
+++ iot_data/models.py
@@ -3,8 +3,6 @@
 from django.db import models
 from channels.models import Channel,User
 
-# Create your models here.
-
 class IotData(models.Model):
     channel = models.ForeignKey(Channel,on_delete=models.CASCADE,related_name='iotdata')
     timestamp = models.DateTimeField(auto_now_add=True)
@@ -14,9 +12,6 @@ class IotData(models.Model):
     field_num = models.PositiveSmallIntegerField()
     data = models.DecimalField(max_digits=19, decimal_places=10)
     uploaded_by = models.ForeignKey(User,on_delete=models.CASCADE,related_name='iotdata')
-    
-    # unclear to ZK what this is for? when could it be entered false?
-    upload_success = models.BooleanField(default=True)
 
     def __str__(self):
         return f'{self.created_at} channel: {self.channel_num} field: {self.field_num} data: {self.data}'
