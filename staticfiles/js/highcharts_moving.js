55e75d325f498e6434f7880bfa7e4799705f69c6
SYAM101u13 <SYAM101u13@ad.pcc.edu>
2019-04-18 09:56:21 -0700

    modified sidebar menu text

---
+++ staticfiles/js/highcharts_moving.js
@@ -0,0 +1,68 @@
+Highcharts.chart('container', {
+    chart: {
+      type: 'spline',
+      animation: Highcharts.svg, // don't animate in old IE
+      marginRight: 10,
+      events: {
+        load: function () {
+  
+          // set up the updating of the chart each second
+          var series = this.series[0];
+          setInterval(function () {
+            var x = (new Date()).getTime(), // current time
+              y = Math.random();
+            series.addPoint([x, y], true, true);
+          }, 1000);
+        }
+      }
+    },
+  
+    time: {
+      useUTC: false
+    },
+  
+    title: {
+      text: 'Live random data'
+    },
+    xAxis: {
+      type: 'datetime',
+      tickPixelInterval: 150
+    },
+    yAxis: {
+      title: {
+        text: 'Value'
+      },
+      plotLines: [{
+        value: 0,
+        width: 1,
+        color: '#808080'
+      }]
+    },
+    tooltip: {
+      headerFormat: '<b>{series.name}</b><br/>',
+      pointFormat: '{point.x:%Y-%m-%d %H:%M:%S}<br/>{point.y:.2f}'
+    },
+    legend: {
+      enabled: false
+    },
+    exporting: {
+      enabled: false
+    },
+    series: [{
+      name: 'Random data',
+      data: (function () {
+        // generate an array of random data
+        var data = [],
+          time = (new Date()).getTime(),
+          i;
+  
+        for (i = -19; i <= 0; i += 1) {
+          data.push({
+            x: time + i * 1000,
+            y: Math.random()
+          });
+        }
+        return data;
+      }())
+    }]
+  });
