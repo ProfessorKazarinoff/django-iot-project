ada85e409e2dbed7bcd9cdc16793cb3a0e50c4eb
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2019-04-17 21:49:44 -0700

    collected static

--- django_iot_project/settings.py
+++ django_iot_project/settings.py
@@ -138,3 +138,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
