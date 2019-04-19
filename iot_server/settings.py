bec3c336c8135bbd094f3d94dd3d09d0c1d0aa34
Peter Kazarinoff <peter.kazarinoff@gmail.com>
2019-04-18 19:50:37 -0700

    heroku staging in Procfile and added whitenoise middleware

--- iot_server/settings.py
+++ iot_server/settings.py
@@ -54,6 +54,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # for Heroku
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = "iot_server.urls"
